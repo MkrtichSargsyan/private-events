4815e6b9a94a008f974b8e2e6f058ffc7bf96d8d
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-11-30 16:39:34 +0400

    finish navigation bar styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def show_user_name(_current_user)
+    if signed_in?
+      "<li class='nav-item'>
+        Logged in as [<strong><%= current_user.email %></strong>]
+      </li>
+      <li class='nav-item'>
+         #{link_to 'logout', destroy_user_session_path, class: 'nav-link text-white', method: :delete}
+      </li>".html_safe
+    else
+      "<li class='nav-item'>
+        #{link_to 'Log in / Sign up', new_user_registration_path, class: 'nav-link text-white'}
+      </li>".html_safe
+      end
+  end
 end
